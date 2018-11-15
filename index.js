41a72cf77a2952d9c9541e081c3f722e127f4050
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-15 21:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(range){
   return function(finalRange){
     if (range >= finalRange){
      let diff = range - finalRange
-     return `${diff} blocks within range`
+     return `within range by ${diff} blocks`
      }
      else { let diff = finalRange- range
      return `${diff} blocks out of range`
