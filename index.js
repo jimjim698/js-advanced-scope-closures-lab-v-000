b84c19481181d03f349f2dc83a4e723a538c25b7
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-15 21:42:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(range){
+  return function(start,finish){
+    let a = parseFloat(start)
+    let b = parseFloat(finish)
+    let finalRange = a - b >0 ? a - b : b - a
+    if (range >= finalRange){
+     let diff = range - finalRange
+     return `within range by ${diff}`
+     }
+     else { let diff = finalRange- range
+     return `${diff} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(percent){
+  return function(fare){
+    return fare * percent
+  }
+}
+
+function createDriver(){
+  let driverId = 0
+  return class{
+    constructor(name){
+      this.name = name 
+    this.id = ++driverId
+    }
+  }
+  
+}
